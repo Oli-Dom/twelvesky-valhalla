ebeb88bfefb19b59d20d0686881099ac9e4c7c6b
Oli-Dom <oadl12345678@gmail.com>
2025-04-24 21:37:40 -0400

    added login wrapper for suspense build issue

---
+++ app/login/LoginFormWrapper.tsx
@@ -0,0 +1,21 @@
+// components/LoginFormWrapper.tsx
+"use client"
+
+import { useSearchParams } from "next/navigation"
+import LoginPage from "./LoginPage"
+
+export default function LoginFormWrapper() {
+  const searchParams = useSearchParams()
+
+  const registered = searchParams.get("registered")
+  const error = searchParams.get("error")
+  const callbackUrl = searchParams.get("callbackUrl") || "/profile"
+
+  return (
+    <LoginPage
+      registered={registered}
+      error={error}
+      callbackUrl={callbackUrl}
+    />
+  )
+}
