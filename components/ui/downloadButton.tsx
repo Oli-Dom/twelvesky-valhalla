9e9f3740292fa094f648e52ae875d3c3b3d0e588
Oli-Dom <oadl12345678@gmail.com>
2025-05-16 22:13:49 -0400

    added download link

--- components/ui/downloadButton.tsx
+++ components/ui/downloadButton.tsx
@@ -4,15 +4,15 @@ import { Download } from "lucide-react";
 
 export function DownloadButton() {
   return (
-    // <Link
-    //   href="https://drive.google.com/uc?export=download&id=1i64c4x2GHW6F38KLIDGLDALUV_wyxDeU"
-    //   target="_blank"
-    //   rel="noopener noreferrer"
-    // >
+    <Link
+      href="https://drive.google.com/uc?export=download&id=1i64c4x2GHW6F38KLIDGLDALUV_wyxDeU"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <Button size="lg" className="fire-button animate-pulse-gold">
         <Download className="mr-2 h-5 w-5" />
         Download Now
       </Button>
-    // </Link>
+     </Link>
   );
 }
